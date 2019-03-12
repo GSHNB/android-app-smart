456d674a44ebba9911e9524768e9741eeaa6303a
gaoshun <gaoshun@ayibang.com>
2019-03-12 16:54:26 +0800

    删除rxjava

--- app/src/main/java/com/nsxz/smart/view/MainActivity.java
+++ app/src/main/java/com/nsxz/smart/view/MainActivity.java
@@ -6,24 +6,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.nsxz.smart.R;
-import com.nsxz.smart.net.request.IGetRequest;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,76 +22,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 //        sendBroadcast();
-        Observable novel = Observable.create(new ObservableOnSubscribe<String>() {
-            @Override
-            public void subscribe(ObservableEmitter<String> observableEmitter) throws Exception {
-                observableEmitter.onNext("1");
-                observableEmitter.onNext("2");
-                observableEmitter.onComplete();
-            }
-        });
+//        Observable novel = Observable.create(new ObservableOnSubscribe<String>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<String> observableEmitter) throws Exception {
+//                observableEmitter.onNext("1");
+//                observableEmitter.onNext("2");
+//                observableEmitter.onComplete();
+//            }
+//        });
 
 
-        Observer<String> reader = new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable disposable) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        };
-
-        novel.subscribe(reader);
-
-        Observable.create(
-                new ObservableOnSubscribe<Object>() {
-                    @Override
-                    public void subscribe(ObservableEmitter<Object> observableEmitter) throws Exception {
-
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Consumer<Object>() {
-                    @Override
-                    public void accept(Object o) throws Exception {
-
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-
-                    }
-                });
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.8:8080/RetrofitService/")
-                .build();
-        IGetRequest requestService = retrofit.create(IGetRequest.class);
-        requestService.getNews(1).enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable throwable) {
-
-            }
-        });
+//        Observer<String> reader = new Observer<String>() {
+//            @Override
+//            public void onSubscribe(Disposable disposable) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable throwable) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        };
+//
+//        novel.subscribe(reader);
+//
+//        Observable.create(
+//                new ObservableOnSubscribe<Object>() {
+//                    @Override
+//                    public void subscribe(ObservableEmitter<Object> observableEmitter) throws Exception {
+//
+//                    }
+//                })
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(new Consumer<Object>() {
+//                    @Override
+//                    public void accept(Object o) throws Exception {
+//
+//                    }
+//                }, new Consumer<Throwable>() {
+//                    @Override
+//                    public void accept(Throwable throwable) throws Exception {
+//
+//                    }
+//                });
+//
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("http://192.168.1.8:8080/RetrofitService/")
+//                .build();
+//        IGetRequest requestService = retrofit.create(IGetRequest.class);
+//        requestService.getNews(1).enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> call, Throwable throwable) {
+//
+//            }
+//        });
 
 
     }
